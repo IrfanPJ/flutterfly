0ed9b883e3130861a0c1fffbc51a7e9a77af7ef4
irfanpj <irfanrichus@gmail.com>
2024-07-04 02:16:18 +0530

    commiting the code

---
+++ Details.dart
@@ -0,0 +1,10 @@
+void main()
+{
+    String name="irfan pj";
+    int age=19;
+    String branch = "ARTIFICIAL INTELLIGENCE";
+
+    print("Name:$name");
+    print("Age:$age");
+    print("Branch:$branch");
+}
